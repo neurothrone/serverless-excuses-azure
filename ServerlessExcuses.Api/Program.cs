d79640d7ea8b8cfaa3ab088e77f77fb1452c9d65
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-05-10 17:13:42 +0200

    add null checks for Cosmos DB environment variables

--- ServerlessExcuses.Api/Program.cs
+++ ServerlessExcuses.Api/Program.cs
@@ -8,8 +8,10 @@ var builder = FunctionsApplication.CreateBuilder(args);
 // Add Cosmos DB client
 builder.Services.AddSingleton(_ =>
 {
-    var endpoint = Environment.GetEnvironmentVariable("CosmosDbEndpoint");
-    var key = Environment.GetEnvironmentVariable("CosmosDbKey");
+    var endpoint = Environment.GetEnvironmentVariable("CosmosDbEndpoint") ??
+                   throw new InvalidOperationException("CosmosDbEndpoint is not set.");
+    var key = Environment.GetEnvironmentVariable("CosmosDbKey") ??
+              throw new InvalidOperationException("CosmosDbKey is not set.");
 
     var cosmosClientOptions = new CosmosClientOptions
     {
